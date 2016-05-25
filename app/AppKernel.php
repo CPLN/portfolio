2c8dd33632f4f64286f1363b9f9d9b81a6817eec
Steeve Droz <steeve.droz@rpn.ch>
2016-05-25 18:45:07 +0200

    Installation de Sluggable

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new PortfolioBundle\PortfolioBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
