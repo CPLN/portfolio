164a3b4d0f656c0ead0af69dadceec0e81d726f4
Steeve Droz <steeve.droz@rpn.ch>
2016-05-25 06:47:30 +0200

    Création de Task et Domain

---
+++ src/PortfolioBundle/Entity/Domain.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace PortfolioBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Domain
+ *
+ * @ORM\Table(name="domain")
+ * @ORM\Entity(repositoryClass="PortfolioBundle\Repository\DomainRepository")
+ */
+class Domain
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="PortfolioBundle\Entity\Task", mappedBy="domain")
+     */
+    private $tasks;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Domain
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->tasks = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add task
+     *
+     * @param \PortfolioBundle\Entity\Task $task
+     *
+     * @return Domain
+     */
+    public function addTask(\PortfolioBundle\Entity\Task $task)
+    {
+        $this->tasks[] = $task;
+
+        return $this;
+    }
+
+    /**
+     * Remove task
+     *
+     * @param \PortfolioBundle\Entity\Task $task
+     */
+    public function removeTask(\PortfolioBundle\Entity\Task $task)
+    {
+        $this->tasks->removeElement($task);
+    }
+
+    /**
+     * Get tasks
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getTasks()
+    {
+        return $this->tasks;
+    }
+}
---
+++ src/PortfolioBundle/Entity/Task.php
@@ -0,0 +1,249 @@
+<?php
+
+namespace PortfolioBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="task")
+ * @ORM\Entity(repositoryClass="PortfolioBundle\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="goal", type="string", length=255)
+     */
+    private $goal;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="duration", type="integer")
+     */
+    private $duration;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="material", type="text", nullable=true)
+     */
+    private $material;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="level", type="integer")
+     */
+    private $level;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="obligatory", type="boolean")
+     */
+    private $obligatory;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="PortfolioBundle\Entity\Domain", inversedBy="tasks")
+     */
+    private $domain;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Task
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set goal
+     *
+     * @param string $goal
+     *
+     * @return Task
+     */
+    public function setGoal($goal)
+    {
+        $this->goal = $goal;
+
+        return $this;
+    }
+
+    /**
+     * Get goal
+     *
+     * @return string
+     */
+    public function getGoal()
+    {
+        return $this->goal;
+    }
+
+    /**
+     * Set duration
+     *
+     * @param integer $duration
+     *
+     * @return Task
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * Get duration
+     *
+     * @return int
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Set material
+     *
+     * @param string $material
+     *
+     * @return Task
+     */
+    public function setMaterial($material)
+    {
+        $this->material = $material;
+
+        return $this;
+    }
+
+    /**
+     * Get material
+     *
+     * @return string
+     */
+    public function getMaterial()
+    {
+        return $this->material;
+    }
+
+    /**
+     * Set level
+     *
+     * @param integer $level
+     *
+     * @return Task
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return int
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Set obligatory
+     *
+     * @param boolean $obligatory
+     *
+     * @return Task
+     */
+    public function setObligatory($obligatory)
+    {
+        $this->obligatory = $obligatory;
+
+        return $this;
+    }
+
+    /**
+     * Get obligatory
+     *
+     * @return bool
+     */
+    public function getObligatory()
+    {
+        return $this->obligatory;
+    }
+
+    /**
+     * Set domain
+     *
+     * @param \PortfolioBundle\Entity\Domain $domain
+     *
+     * @return Task
+     */
+    public function setDomain(\PortfolioBundle\Entity\Domain $domain = null)
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
+    /**
+     * Get domain
+     *
+     * @return \PortfolioBundle\Entity\Domain
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+}
